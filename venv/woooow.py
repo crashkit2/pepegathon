0d96f227a3a7686836fd9a10f16e0c0f81456844
crashkit2 <crashkit2@gmail.com>
2019-05-18 23:21:41 +0300

    .csv converter from .log (done)

---
+++ venv/woooow.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import csv
+
+ip_file = (r'website-access.log')
+op_file = (ip_file.split("."))
+
+print("Opening " + ip_file)
+
+f0 = open(ip_file, "r")
+lstInput = []
+for oLine in f0:
+    try:
+        lstLine = oLine.replace("\n", "").split("Info type:")
+    except Exception as e:
+        print(e)
+        pass
+    lstInput.append(lstLine)
+
+f0.close()
+
+fw = open(op_file[0] + ".csv", "w")
+for oLine in lstInput:
+    szWriteLine = ",".join(oLine)
+    fw.write(szWriteLine + "\n")
+fw.close()
