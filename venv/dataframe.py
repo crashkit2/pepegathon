46cb3b65ca40315cedab8adf3e61deab7eb7c155
crashkit2 <crashkit2@gmail.com>
2019-05-19 00:36:25 +0300

    sum of dataframe traffic bytes (done) different ips found (20) (done)
    Added support for any kind of .csv file analysis (Traffic and different IP hosts)

--- venv/dataframe.py
+++ venv/dataframe.py
@@ -4,7 +4,10 @@ file=(r'biglog.csv')
 cols=['host','1','user_id','date','tz','endpoint','status','data','referer','user_agent']
 df=pd.read_csv(file,delim_whitespace=True,names=cols).drop('1',1)
 print(df.head())
+#traffic analysis
+g=sum(df.data)
+print ('Traffic sum = ',g)
 
-sum1= sum(df.data)
+df=pd.unique(df.host)
+print ('Different IPs found (20): ',df)
 #summary of data collumn
-print (sum1)
