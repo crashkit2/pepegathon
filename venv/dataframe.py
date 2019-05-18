edbd3af5cde27872b345bd27b2c81fd63625a529
crashkit2 <crashkit2@gmail.com>
2019-05-19 00:53:10 +0300

    sum of dataframe traffic bytes (done) different ips found (20) (done)
    Added support for any kind of .csv file analysis (Traffic and different IP hosts)
    Also added 5xx error finder

--- venv/dataframe.py
+++ venv/dataframe.py
@@ -4,10 +4,18 @@ file=(r'biglog.csv')
 cols=['host','1','user_id','date','tz','endpoint','status','data','referer','user_agent']
 df=pd.read_csv(file,delim_whitespace=True,names=cols).drop('1',1)
 print(df.head())
-#traffic analysis
+#traffic analysis/bytes summary
 g=sum(df.data)
 print ('Traffic sum = ',g)
 
+#Requests regarding 5xx errors
+count = 0
+for line in df.status:
+    if line >=500 and line <=600:
+        count=count+1
+
+print ('5xx Errors found : ',count)
+
+#IP organizer
 df=pd.unique(df.host)
 print ('Different IPs found (20): ',df)
-#summary of data collumn
